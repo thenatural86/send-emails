d5e278df3fc406bf06b6d0e2ba3eabee1d47d7c9
thenatural86 <philip.smalls86@gmail.com>
2024-04-08 10:14:45 -0300

    email logic

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,5 +1,26 @@
+const nodemailer = require('nodemailer')
+
 const sendEmail = async (req, res) => {
-  res.send('send email func')
+  let testAccount = await nodemailer.createTestAccount()
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'addison16@ethereal.email',
+      pass: '2tkBXjX1dx2j393VxJ',
+    },
+  })
+
+  const info = await transporter.sendMail({
+    from: '"Test Person" <testperson@gmail.com>',
+    to: 'bar@exmple.com',
+    subject: 'Hello World',
+    text: 'Hello World',
+    html: '<h2>Sending Emails with Node.js',
+  })
+
+  res.json(info)
 }
 
 module.exports = sendEmail
